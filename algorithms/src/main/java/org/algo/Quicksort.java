aacce66e392a1b9946595f96e1043e326025bf2a
admin <m.g.kuznetsov@gmail.com>
2020-04-14 23:03:54 -0400

    Add quicksort implementation

---
+++ algorithms/src/main/java/org/algo/Quicksort.java
@@ -0,0 +1,58 @@
+package org.algo;
+
+public class Quicksort {
+
+    public void sort(int[] arr) {
+        sort(arr, 0, arr.length-1);
+    }
+
+    public void sort(int[] arr, int startIndex, int endIndex) {
+
+        // Base case
+        if(endIndex-startIndex==0 || startIndex > endIndex) {
+            return;
+        }
+
+        //Choose pivot and pre-process:
+        placePivot(arr, startIndex, endIndex);
+        int pivotIndex = startIndex;
+        int pivotValue = arr[pivotIndex];
+
+        //Partition:
+        //j points to the first element that is > pivot
+
+        for(int j=startIndex; j<=endIndex; j++) {
+            if(arr[j] < pivotValue) {
+                // Swap so that all elements before and including j are > pivot
+                // Element after pivotIndex will now be less than pivot
+                swap(arr, pivotIndex+1, j); 
+
+                // Swap pivot and element after pivot.
+                // Now all elements before pivot are < pivot
+                swap(arr,pivotIndex+1,pivotIndex); 
+                pivotIndex++;
+            }
+        }
+
+        //Recourse:
+        sort(arr, startIndex, pivotIndex-1); // Sort everything less than pivot
+        sort(arr, pivotIndex+1, endIndex);
+    }
+
+    public void placePivot(int[] arr, int startIndex, int endIndex) {
+        int pivotIndex = getPivotIndex(startIndex, endIndex);
+        swap(arr, startIndex, pivotIndex); //Swap pivot with the first element of array
+    }
+
+    public int getPivotIndex(int startIndex, int endIndex) {
+        //return startIndex; //For now return the first element of the array
+        return startIndex + (int) (Math.random() * (endIndex - startIndex + 1));
+    }
+
+    public void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+}
---
+++ algorithms/src/test/java/org/algo/QuickSortTest.java
@@ -0,0 +1,55 @@
+package org.algo;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import org.junit.Test;
+
+public class QuickSortTest 
+{
+
+    @Test
+    public void testQuickSortEvenNumElementsReverseOrder()
+    {
+        final Quicksort quickSort = new Quicksort();
+
+        final int[] arr1 = { 4, 3, 2, 1 };
+        quickSort.sort(arr1);
+        final int[] expectedArr1 = { 1, 2, 3, 4 };
+        assertTrue(Arrays.equals(arr1, expectedArr1));
+    }
+
+    @Test
+    public void testQuickSortOddNumElementsReverseOrder()
+    {
+        final Quicksort quickSort = new Quicksort();
+
+        final int[] arr1 = { 5, 4, 3, 2, 1 };
+        quickSort.sort(arr1);
+        final int[] expectedArr1 = { 1, 2, 3, 4, 5 };
+        assertTrue(Arrays.equals(arr1, expectedArr1));
+    }
+
+    @Test
+    public void testQuickSortOrdered()
+    {
+        final Quicksort quickSort = new Quicksort();
+
+        final int[] arr1 = { 1, 2, 3, 4, 5 };
+        quickSort.sort(arr1);
+        final int[] expectedArr1 = { 1, 2, 3, 4, 5 };
+        assertTrue(Arrays.equals(arr1, expectedArr1));
+    }
+
+
+    @Test
+    public void testQuickSortJumbledArray()
+    {
+        final Quicksort quickSort = new Quicksort();
+
+        final int[] arr1 = { 3, 8, 2, 2, 5, 1, 4, 4, 7, 1 };
+        quickSort.sort(arr1);
+        final int[] expectedArr1 = { 1, 1, 2, 2, 3, 4, 4, 5, 7, 8 };
+        assertTrue(Arrays.equals(arr1, expectedArr1));
+    }
+}
